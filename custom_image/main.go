ff4e008edd8f4bcc138051980fc53ece4075c82a
Tino Adams <tiadams@thoughtworks.com>
2015-07-10 10:20:49 +1000

    Using golint during build process

--- custom_image/main.go
+++ custom_image/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func Foo(bar int) int {
+    if bar > 0 {
+        return 123
+    } else {
+        return 456
+    }
+}
+
 func main() {
 	fmt.Println("Hello, 世界")
 }
